bcc660a165cf32f2efb9e2d292a0a7f819e16c8a
Darrin Holst <darrinholst@gmail.com>
2010-09-27 13:37:21 -0500

    show when stuff is read/shared/starred

--- app/assistants/article-assistant.js
+++ app/assistants/article-assistant.js
@@ -7,7 +7,19 @@ var ArticleAssistant = Class.create(BaseAssistant, {
   ready: function($super) {
     $super()
     this.controller.get("title").update(this.article.title)
-    this.controller.get("author").update("by Dan Mullins")
+    this.controller.get("author").update(this.article.author ? "by " + this.article.author : this.article.origin)
     this.controller.get("summary").update(this.article.summary)
+
+    if(this.article.isRead) {
+      this.controller.get("read").addClassName("on")
+    }
+    
+    if(this.article.isStarred) {
+      this.controller.get("starred").addClassName("on")
+    }
+
+    if(this.article.isShared) {
+      this.controller.get("shared").addClassName("on")
+    }
   }
 })
--- app/models/article.js
+++ app/models/article.js
@@ -1,9 +1,11 @@
 var Article = Class.create({
   initialize: function(data) {
     this.title = data.title
+    this.author = data.author
+    this.origin = data.origin ? data.origin.title : null
     var content = data.content || data.summary
     this.summary = this.cleanUp(content.content)
-    this.setReadState(data.categories)
+    this.setStates(data.categories)
     this.setDates(parseInt(data.crawlTimeMsec))
   },
   
@@ -14,14 +16,23 @@ var Article = Class.create({
     return cleaned
   },
   
-  setReadState: function(categories) {
+  setStates: function(categories) {
     this.isRead = false
+    this.isShared = false
+    this.isStarred = false
     
     categories.each(function(category) {
       if(category.endsWith("/state/com.google/read")) {
         this.isRead = true
-        return
       }
+      
+      if(category.endsWith("/state/com.google/starred")) {
+        this.isStarred = true
+      }
+
+      if(category.endsWith("/state/com.google/broadcast")) {
+        this.isShared = true
+      }       
     }.bind(this))
   },
   
