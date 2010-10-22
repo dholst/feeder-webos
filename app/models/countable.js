1b15355b5c0a2db0c1957bee381a1ba93d33afb0
Darrin Holst <darrinholst@gmail.com>
2010-10-22 08:00:10 -0500

    don't let count go negative

--- app/models/countable.js
+++ app/models/countable.js
@@ -2,30 +2,30 @@ var Countable = Class.create({
   initialize: function() {
     this.unreadCount = 0
   },
-  
+
   clearUnreadCount: function() {
     this.setUnreadCount(0)
   },
-  
+
   setUnreadCount: function(count) {
     this.unreadCount = count
-    
+
     if(this.unreadCount < 0) {
       this.unreadCount = 0
     }
-    
+
     this.unreadCountDisplay = count > 999 ? "1000+" : count
-    this.unreadCountDisplay = count == 0 ? "" : this.unreadCountDisplay
+    this.unreadCountDisplay = count <= 0 ? "" : this.unreadCountDisplay
   },
-  
+
   incrementUnreadCountBy: function(count) {
     this.setUnreadCount((this.getUnreadCount() || 0) + count)
   },
-  
+
   decrementUnreadCountBy: function(count) {
     this.incrementUnreadCountBy(-count)
   },
-  
+
   getUnreadCount: function() {
     return this.unreadCount
   }
