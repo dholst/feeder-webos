de5c006e6d785087daeda8b05e0abb59c6933119
Darrin Holst <darrinholst@gmail.com>
2010-10-27 07:58:32 -0500

    fix for articles with no urls

--- app/models/article-container.js
+++ app/models/article-container.js
@@ -3,35 +3,35 @@ var ArticleContainer = Class.create(Countable, {
     $super()
     this.api = api
     this.continuation = false
-    this.items = [] 
+    this.items = []
   },
 
   reset: function() {
     this.items.clear()
     this.continuation = false
   },
-  
+
   findArticles: function(success, failure) {
     var onSuccess = function(articles, id, continuation) {
       Log.debug("continuation token is " + continuation)
-      
+
       this.continuation = continuation
-      
+
       if(this.items.length && this.items[this.items.length - 1].load_more) {
         this.items.pop()
       }
-      
+
       $A(articles).each(function(articleData) {
         this.items.push(new Article(articleData, this))
       }.bind(this))
-      
+
       if(this.continuation) {
         this.items.push(new LoadMore())
       }
-      
+
       success()
     }.bind(this)
-    
+
     this.makeApiCall(this.continuation, onSuccess, failure)
   }
 })
