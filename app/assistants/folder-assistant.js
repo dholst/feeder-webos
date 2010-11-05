1898ad7c73069fee7485c5fd23eb68b40a380022
Darrin Holst <darrinholst@gmail.com>
2010-11-05 15:37:32 -0500

    reorder sorting of folders

--- app/assistants/folder-assistant.js
+++ app/assistants/folder-assistant.js
@@ -2,24 +2,27 @@ var FolderAssistant = Class.create(BaseAssistant, {
   initialize: function($super, folder) {
     $super()
     this.folder = folder
+    this.subscriptions = {items: []}
   },
 
   setup: function($super) {
     $super()
+    this.filter()
+
+    console.log(this.subscriptions.items.length)
     this.controller.setupWidget("folders", {
       itemTemplate: "folder/folder",
       onItemRendered: this.folderRendered,
-      itemsProperty: "subscriptions",
       reorderable: Preferences.isManualFeedSort(),
   		swipeToDelete: true
-    }, this.folder)
+    }, this.subscriptions)
 
     this.controller.setupWidget("sticky-folders", {itemTemplate: "folder/folder", onItemRendered: this.folderRendered, itemsProperty: "stickySubscriptions"}, this.folder)
     this.controller.listen("sticky-folders", Mojo.Event.listTap, this.folderTaped = this.folderTapped.bind(this))
     this.controller.listen("folders", Mojo.Event.listTap, this.folderTaped)
     this.controller.listen("folders", Mojo.Event.listReorder, this.sourcesReordered = this.sourcesReordered.bind(this))
     this.controller.listen("folders", Mojo.Event.listDelete, this.sourceDeleted = this.sourceDeleted.bind(this))
-    this.controller.listen(document, "SubscriptionDeleted", this.refresh = this.refresh.bind(this))
+    this.controller.listen(document, "SubscriptionDeleted", this.filterAndRefresh = this.filterAndRefresh.bind(this))
   },
 
   cleanup: function($super) {
@@ -28,7 +31,7 @@ var FolderAssistant = Class.create(BaseAssistant, {
     this.controller.stopListening("folders", Mojo.Event.listTap, this.folderTaped)
     this.controller.stopListening("folders", Mojo.Event.listReorder, this.sourcesReordered)
     this.controller.stopListening("folders", Mojo.Event.listDelete, this.sourceDeleted)
-    this.controller.stopListening(document, "SubscriptionDeleted", this.refresh)
+    this.controller.stopListening(document, "SubscriptionDeleted", this.filterAndRefresh)
   },
 
   ready: function($super) {
@@ -37,18 +40,29 @@ var FolderAssistant = Class.create(BaseAssistant, {
 
   activate: function($super, changes) {
     $super(changes)
-    this.refresh()
+    this.filterAndRefresh()
   },
 
-  refresh: function() {
+  filterAndRefresh: function() {
+    this.filter()
     this.refreshList(this.controller.get("sticky-folders"), this.folder.stickySubscriptions)
-    this.refreshList(this.controller.get("folders"), this.folder.subscriptions.items)
+    this.refreshList(this.controller.get("folders"), this.subscriptions.items)
 
-    if(!this.folder.subscriptions.items.length) {
+    if(!this.subscriptions.items.length) {
       this.controller.stageController.popScene()
-    }    
+    }
   },
-  
+
+  filter: function() {
+    this.subscriptions.items.clear()
+
+    this.folder.subscriptions.items.each(function(subscription) {
+      if(subscription.unreadCount || !Preferences.hideReadFeeds()) {
+        this.subscriptions.items.push(subscription)
+      }
+    }.bind(this))
+  },
+
   folderTapped: function(event) {
     this.controller.stageController.pushScene("articles", event.item)
   },
@@ -61,22 +75,19 @@ var FolderAssistant = Class.create(BaseAssistant, {
     if(itemModel.unreadCount) {
       $(itemNode).addClassName("unread")
     }
-    else if(Preferences.hideReadFeeds() && !itemModel.sticky) {
-      itemNode.hide()
-    }
   },
 
   sourcesReordered: function(event) {
     var beforeSubscription = null
 
-    if(event.toIndex < this.folder.subscriptions.items.length - 1) {
+    if(event.toIndex < this.subscriptions.items.length - 1) {
       var beforeIndex = event.toIndex
 
       if(event.fromIndex < event.toIndex) {
         beforeIndex += 1
       }
 
-      beforeSubscription = this.folder.subscriptions.items[beforeIndex]
+      beforeSubscription = this.subscriptions.items[beforeIndex]
     }
 
     this.folder.subscriptions.move(event.item, beforeSubscription)
