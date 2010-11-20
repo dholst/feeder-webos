4b2b889c9dbe0e5495fa43d7bfdfa983ae94e323
Darrin Holst <darrinholst@gmail.com>
2010-11-20 13:35:13 -0600

    launch app when dashboard tapped

--- app/assistants/dashboard-assistant.js
+++ app/assistants/dashboard-assistant.js
@@ -7,8 +7,18 @@ var DashboardAssistant = Class.create({
     this.controller.update("dashboard-title", $L("New Articles"))
     this.controller.update("dashboard-text", $L("You have new articles to read"))
     this.controller.update("unread-count", this.count)
+    this.controller.listen("dashboard-info", Mojo.Event.tap, this.launchApp = this.launchApp.bind(this))
   },
 
+  cleanup: function() {
+    this.controller.stopListening("dashboard-info", Mojo.Event.tap, this.launchApp)
+  },
+
+  launchApp: function() {
+    Mojo.Controller.getAppController().assistant.handleLaunch()
+    this.controller.window.close()  
+  },
+    
   updateDashboard: function(count) {
     this.controller.update("unread-count", count)                     
   }
