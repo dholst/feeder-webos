7910b8074651465ad0f015d106246d482d65e466
Darrin Holst <darrinholst@gmail.com>
2010-11-21 19:23:53 -0600

    fix theme

--- app/assistants/app-assistant.js
+++ app/assistants/app-assistant.js
@@ -1,38 +1,38 @@
 var AppAssistant = Class.create({
-	initialize: function() {
-		this.mainStageName = "MainStage"
+  initialize: function() {
+    this.mainStageName = "MainStage"
     this.dashboardStageName = "DashboardStage"
-	},
+  },
 
-	setup: function() {
-		this.setInterval()
-	},
+  setup: function() {
+    this.setInterval()
+  },
 
-	handleLaunch: function(parameters) {
-		Log.debug("App Launched with " + Object.toJSON(parameters))
-		var appController = Mojo.Controller.getAppController()
-		var cardStageController = this.controller.getStageController(this.mainStageName)
+  handleLaunch: function(parameters) {
+    Log.debug("App Launched with " + Object.toJSON(parameters))
+    var appController = Mojo.Controller.getAppController()
+    var cardStageController = this.controller.getStageController(this.mainStageName)
 
-		if (parameters) {
+    if (parameters) {
       if(parameters.action == "update") {
         this.checkForUpdates()
       }
 
-			this.setInterval(parameters.action == "notificationIntervalChange")
-		}
-		else {
-			if (cardStageController) {
-				cardStageController.activate()
-			}
-			else {
-				this.controller.createStageWithCallback(
+      this.setInterval(parameters.action == "notificationIntervalChange")
+    }
+    else {
+      if (cardStageController) {
+        cardStageController.activate()
+      }
+      else {
+        this.controller.createStageWithCallback(
           {name: this.mainStageName, lightweight: true},
-				  function(stageController) {stageController.pushScene("login")},
-				  "card"
+          function(stageController) {stageController.pushScene("login")},
+          "card"
         )
-			}
-		}
-	},
+      }
+    }
+  },
 
   checkForUpdates: function() {
     var self = this
@@ -71,41 +71,36 @@ var AppAssistant = Class.create({
     }
   },
 
-	setInterval: function(changed) {
+  setInterval: function(changed) {
     var self = this
 
-		if (Preferences.notificationInterval() == "00:00:00" || changed) {
-			new Mojo.Service.Request("palm://com.palm.power/timeout", {
-				method: "clear",
+    if (Preferences.notificationInterval() == "00:00:00" || changed) {
+      new Mojo.Service.Request("palm://com.palm.power/timeout", {
+        method: "clear",
 
-				parameters: {
-					"key": Mojo.appInfo.id + ".timer",
-				}
-			})
-		}
+        parameters: {
+          "key": Mojo.appInfo.id + ".timer",
+        }
+      })
+    }
 
-		if (Preferences.notificationInterval() != "00:00:00") {
-      if(!self.wakeupRequest) {
-        self.checkForUpdates()
-      }
-        
-			self.wakeupRequest = new Mojo.Service.Request("palm://com.palm.power/timeout", {
-				method: "set",
-
-				parameters: {
-					"key": Mojo.appInfo.id + ".timer",
-					"in": Preferences.notificationInterval(),
-					"wakeup": true,
-					"uri": "palm://com.palm.applicationManager/open",
-					"params": {
-						"id": Mojo.appInfo.id,
-						"params": {
-							"action": "update"
-						}
-					}
-				}
-			})
-		}
-	}
+    if (Preferences.notificationInterval() != "00:00:00") {
+      self.wakeupRequest = new Mojo.Service.Request("palm://com.palm.power/timeout", {
+        method: "set",
+        parameters: {
+          "key": Mojo.appInfo.id + ".timer",
+          "in": Preferences.notificationInterval(),
+          "wakeup": true,
+          "uri": "palm://com.palm.applicationManager/open",
+          "params": {
+            "id": Mojo.appInfo.id,
+            "params": {
+              "action": "update"
+            }
+          }
+        }
+      })
+    }
+  }
 })
 
